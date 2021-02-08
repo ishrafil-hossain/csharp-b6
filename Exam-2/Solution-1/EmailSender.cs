bae5998365af1c1399e3dc988674ad1967ce0eb0
Md Ishrafil Hossain <ishrafil2233@gmail.com>
2021-02-09 00:07:24 +0600

    exam-2 solution

---
+++ Exam-2/Solution-1/EmailSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution_1
+{
+    static class EmailSender
+    {
+        public static void Send()
+        {
+
+        }
+    }
+}
---
+++ Exam-2/Solution-1/INode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution_1
+{
+    public interface INode<C,I> //where C is class & I is interface
+    {
+        public void Add<item>()
+        {
+
+        }
+        public void Remove<item>()
+        {
+
+        }
+    }
+}
